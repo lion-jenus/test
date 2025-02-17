819e07ccc905ecc5c2a9230d8fa2bbc9b2280318
전선일 <jeonsunil1225@gmail.com>
2025-02-17 17:25:35 +0900

    인풋에 값을 입력하면, 인풋값이 html에 출력되도록 만들었다.

--- public/src/src.js
+++ public/src/src.js
@@ -1,6 +1,25 @@
 const root = window.document.getElementById('root');
-const dice = root.getElementsByClassName('dice');
+const dice = document.querySelector('.dice');
+const input = document.querySelector('input');
+const btn = document.querySelector('button');
 
 console.log(root);
 console.log(dice);
-console.log(dice[2]);
+console.log(input);
+
+btn.addEventListener('click', function(){
+  const input = document.querySelector('input');
+  if(input.value.trim() === "") {
+    alert('값을 입력해 주세요.')
+  } else {
+    console.log(input.value);
+    const div = document.createElement('div');
+    div.innerText = input.value;
+
+    root.appendChild(div);
+  }
+
+
+
+  return input.value = ""
+});
